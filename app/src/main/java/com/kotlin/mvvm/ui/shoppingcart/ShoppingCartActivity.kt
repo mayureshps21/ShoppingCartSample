4e755210f3f5db257244421fe5d95f34dd884c04
mayureshcostco <c_mdeshmukh@costco.com>
2023-02-06 17:51:29 +0530

    shopping cart activity added.

---
+++ app/src/main/java/com/kotlin/mvvm/ui/shoppingcart/ShoppingCartActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.mvvm.ui.shoppingcart
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.mvvm.R
+
+class ShoppingCartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shopping_cart)
+    }
+}
