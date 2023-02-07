3e18c3105b32ba9de4a45da7c1838afa294a8d93
mayureshcostco <c_mdeshmukh@costco.com>
2023-02-07 09:29:38 +0530

    Code mearge fix.

--- app/src/main/java/com/kotlin/mvvm/data/repository/ValidSessionRepoImpl.kt
+++ app/src/main/java/com/kotlin/mvvm/data/repository/ValidSessionRepoImpl.kt
@@ -1,8 +1,9 @@
 package com.kotlin.mvvm.data.repository
 
+import com.kotlin.mvvm.data.api.ApiInterface
 import com.kotlin.mvvm.domain.repository.ValidSessionRepo
 import com.kotlin.mvvm.utils.ApplicationConstant
-import com.kotlin.mvvm.utils.baseMVVM.ApiInterface
+import com.kotlin.mvvm.utils.DataProvider
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
