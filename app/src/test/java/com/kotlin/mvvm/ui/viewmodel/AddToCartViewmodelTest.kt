5e1a8e32c8476f64613628d62992a9e820d7c8aa
LL\deekandw <deekandw@publicisgroupe.net>
2023-02-09 14:45:48 +0530

    viewmodel refinement

--- app/src/test/java/com/kotlin/mvvm/ui/viewmodel/AddToCartViewmodelTest.kt
+++ app/src/test/java/com/kotlin/mvvm/ui/viewmodel/AddToCartViewmodelTest.kt
@@ -1,17 +1,22 @@
 package com.kotlin.mvvm.ui.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import app.cash.turbine.test
 import com.kotlin.mvvm.domain.useCase.AddItemToCartUseCase
-import com.kotlin.mvvm.domain.useCase.ValidUserSessionUsecase
+import com.kotlin.mvvm.ui.viewState.AddItemToCartState
 import com.kotlin.mvvm.utils.ApplicationConstant
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.test.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.ResponseBody
 import org.junit.*
 import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
+import retrofit2.HttpException
+import retrofit2.Response
 
 
 class AddToCartViewModelTest {
@@ -21,6 +26,7 @@ class AddToCartViewModelTest {
     val product = "wallet"
     val amount = "$75"
     val address = "Washington D.C"
+
     @OptIn(ExperimentalCoroutinesApi::class)
     var dispatcher = TestCoroutineDispatcher()
 
@@ -35,37 +41,57 @@ class AddToCartViewModelTest {
     fun setUp() {
         MockitoAnnotations.initMocks(this)
         Dispatchers.setMain(dispatcher)
-        viewModel = AddToCartViewModel( addItemToCartUseCase)
+        viewModel = AddToCartViewModel(addItemToCartUseCase)
         viewModel.addItemToCartUseCase = addItemToCartUseCase
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun addItemToCartSuccessWhenSessionisValidTest() = runTest(dispatcher) {
+    fun addItemToCartSuccessTest(): Unit = runTest(dispatcher) {
+        val flow=flow { emit(ApplicationConstant.ITEM_ADDED) }
+        Mockito.`when`(addItemToCartUseCase.addItemToCart(id, product, amount, address))
+            .thenReturn(flow)
+        viewModel.addItem.invoke()
+        flow.test {
+            Assert.assertEquals(this.awaitItem(),ApplicationConstant.ITEM_ADDED)
+            awaitComplete()
+        }
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun addItemToCartFailedTest() = runTest(dispatcher) {
+        val flow=flow { emit(ApplicationConstant.ITEM_ADD_ERROR) }
 
         Mockito.`when`(addItemToCartUseCase.addItemToCart(id, product, amount, address))
-            .thenReturn(flow { emit(ApplicationConstant.ITEM_ADDED) })
+            .thenReturn(flow)
         viewModel.addItem.invoke()
-        Mockito.verify(addItemToCartUseCase).addItemToCart(id, product, amount, address)
+        flow.test {
+            Assert.assertEquals(this.awaitItem(),ApplicationConstant.ITEM_ADD_ERROR)
+            awaitComplete()
+        }
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun addItemToCartSuccessWhenSessionExpiredTest() = runTest(dispatcher) {
-        Mockito.`when`(addItemToCartUseCase.addItemToCart(id,product,amount,address))
-            .thenReturn(flow { emit(ApplicationConstant.SESSION_EXPIRED) })
-        viewModel.addItem.invoke()
-        Mockito.verify(addItemToCartUseCase).addItemToCart(id,product,amount,address)
-    }
+//    @OptIn(ExperimentalCoroutinesApi::class)
+//    @Test(expected =HttpException::class)
+//    fun addItemToCartFailedWithExceptionTest()  = runTest {
+//        Mockito.`when`(addItemToCartUseCase.addItemToCart(id, product, amount, address))
+//            .thenThrow(HttpException(
+//                Response.error<Any>(
+//                    404,
+//                    ResponseBody.create("plain/text".toMediaTypeOrNull(), "Something broke!")
+//                )
+//            ))
+//            try{
+//                viewModel.addItem.invoke()
+//            }catch (e:Exception){
+//                Assert.assertEquals("Something broke!", e.message)
+//            }
+//
+//
+//    }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    @Test
-    fun addItemToCartSuccessWhenSessionRefreshedFailedTest() = runTest(dispatcher) {
-        Mockito.`when`(addItemToCartUseCase.addItemToCart(id,product,amount,address))
-            .thenReturn(flow { emit(ApplicationConstant.SESSION_REFRESHED_FAILED) })
-        viewModel.addItem.invoke()
-        Mockito.verify(addItemToCartUseCase).addItemToCart(id,product,amount,address)
-    }
+
 
     @OptIn(ExperimentalCoroutinesApi::class)
     @After
